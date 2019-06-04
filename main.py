929e701a9be668a654c08856a9ee6e9df0805d7f
Melander Daniel <daniel.melander@luke.fi>
2019-06-04 12:00:10 +0300

    cleaning complete

--- main.py
+++ main.py
@@ -3,39 +3,19 @@ import glob
 import os
 import re
 from cliarguments import get_args
-from template import Template
-
-patrnFuncLine = re.compile(".*\s*<-\s*function\(.*\)") # Regex for the function declaration line
-patrnFuncName = re.compile("(.*?)\s*<-\s*function\(.*\)") # Extracting the function names
-patrnFuncArgs = re.compile("function\s*(?:\()(.*)(?:\))") # Extracting the functions arguments from the function line
-patrnFuncVars = re.compile("([a-z]+[0-9|A-Z|_|a-z]*)(?=\s*[,|\n|\r\n]|\s|$)") # Extracts all components except the model coefficients from function agruments
+from template_utils import parseFileAndGenerateTemplate
 
 def getFiles():
     args = get_args() # Solve CLI arguments
     path = args.folder + '\\*'  + args.extension
     return glob.glob(path) # Returns a list of files
 
-def parseFileAndGenerateTemplate(files):
-    templates = list()
-    for f in files:
-        fp = open(f) # Read the file
-        for i, line in enumerate(fp): # Read file line-by-line
-            if patrnFuncLine.match(line):
-                # Extract the file name from the line
-                parsedLine = line.split(" ")[0] # Parse line to the first " "
-                # Extract the components from the arguments
-                strFuncArgs = patrnFuncArgs.search(line).group(1)
-                funcArgs = patrnFuncVars.findall(strFuncArgs)
-                templates.append( Template(parsedLine, funcArgs) )
-        fp.close()
-    return templates
-
 def createMetadataFiles(templates):
     # Generate the output directory
     outputDir = get_args().output
     if not os.path.exists(outputDir):
         os.makedirs(outputDir)
-    # create N number of files based len(fileNames)
+    # create as many output files as we have function declarations in R -files
     for template in templates:
         outputFile = str(outputDir + "\\" + template.title + ".txt")   # Liitä extensio
         f = open(outputFile, "w+")
---
+++ template_utils.py
@@ -0,0 +1,37 @@
+import re
+from template import Template
+
+functionDeclarationPattern = re.compile(".*\s*<-\s*function\(.*\)") # Regex for the function declaration line
+# patrnFuncName = re.compile("(.*?)\s*<-\s*function\(.*\)") # Extracting the function names
+argumentsPattern = re.compile("function\s*(?:\()(.*)(?:\))") # Extracting the functions arguments from the function line
+componentsPattern = re.compile("([a-z]+[0-9|A-Z|_|a-z]*)(?=\s*[,|\n|\r\n]|\s|$)") # Extracts all components except the model coefficients from function agruments
+
+def parseFileAndGenerateTemplate(files):
+    templates = list()
+    for f in files:
+        fp = open(f) # Read the file
+        for i, line in enumerate(fp): # Read file line-by-line
+            if isFunctionDeclaration(line):
+                template = parseLineAndCreateTemplate(line)
+                templates.append(template)
+        fp.close()
+    return templates
+
+def isFunctionDeclaration(line):
+    return functionDeclarationPattern.match(line)
+
+def parseLineAndCreateTemplate(line):
+    title = parseTitle(line)
+    components = parseComponents(line)
+    return Template(title, components)
+
+# TODO: This does not work in all cases. i.e myfunc<-function(a,b) { ... }
+def parseTitle(line):
+    """ Extract the file name from the line which also servers as the title """ 
+    return line.split(" ")[0] # Parse line to the first " "
+
+def parseComponents(line):
+    # Extract the components from the arguments
+    argsStr = argumentsPattern.search(line).group(1)
+    components = componentsPattern.findall(argsStr)
+    return components
